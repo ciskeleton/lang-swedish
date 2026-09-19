2d61dfcd935910412491f4cb3946c0853c3b4a56
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-09-19 19:53:47 +0100

    Initial Commit!

---
+++ src/info.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * CiSkeleton Language Metadata - Swedish
+ *
+ * @package     CiSkeleton\Core
+ * @subpackage  Language
+ * @author      Kader Bouyakoub <bkader[at]mail[dot]com>
+ * @copyright   Copyright (c) 2018-present, Kader Bouyakoub
+ */
+
+return [
+	'name'       => 'Svenska',
+	'name_en'    => 'Swedish',
+	'locale'     => 'sv_SE',
+	'tag'        => 'sv-SE',
+	'direction'  => 'ltr',
+	'code'       => 'sv',
+	'flag'       => 'se',
+	'version'    => '1.0.0',
+	'version_id' => 10000,
+
+	// Localized language names.
+	'localized_names' => [
+		'arabic'     => 'Arabiska',
+		'armenian'   => 'Armeniska',
+		'bengali'    => 'Bengali',
+		'brazilian'  => 'Portugisiska (Brasilien)',
+		'bulgarian'  => 'Bulgariska',
+		'catalan'    => 'Katalanska',
+		'chinese'    => 'Kinesiska (förenklad)',
+		'croatian'   => 'Kroatiska',
+		'czech'      => 'Tjeckiska',
+		'danish'     => 'Danska',
+		'dutch'      => 'Nederländska',
+		'english'    => 'Engelska',
+		'estonian'   => 'Estniska',
+		'filipino'   => 'Filipino',
+		'finnish'    => 'Finska',
+		'french'     => 'Franska',
+		'georgian'   => 'Georgiska',
+		'german'     => 'Tyska',
+		'greek'      => 'Grekiska',
+		'hindi'      => 'Hindi',
+		'hungarian'  => 'Ungerska',
+		'indonesian' => 'Indonesiska',
+		'italian'    => 'Italienska',
+		'japanese'   => 'Japanska',
+		'korean'     => 'Koreanska',
+		'latvian'    => 'Lettiska',
+		'lithuanian' => 'Litauiska',
+		'malay'      => 'Malajiska',
+		'norwegian'  => 'Norska',
+		'persian'    => 'Persiska',
+		'polish'     => 'Polska',
+		'portuguese' => 'Portugisiska',
+		'romanian'   => 'Rumänska',
+		'russian'    => 'Ryska',
+		'serbian'    => 'Serbiska',
+		'slovak'     => 'Slovakiska',
+		'slovenian'  => 'Slovenska',
+		'spanish'    => 'Spanska',
+		'swahili'    => 'Swahili',
+		'swedish'    => 'Svenska',
+		'taiwanese'  => 'Kinesiska (traditionell)',
+		'tamil'      => 'Tamil',
+		'thai'       => 'Thailändska',
+		'turkish'    => 'Turkiska',
+		'ukrainian'  => 'Ukrainska',
+		'urdu'       => 'Urdu',
+		'vietnamese' => 'Vietnamesiska',
+	],
+];
